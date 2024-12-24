078b89d73d7d55b291df1359836caaec776e2681
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-24 12:28:23 +0100

    create footer for index page

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <div class="container">
+            <p>&copy; <?php echo date("Y"); ?> John Doe. All rights reserved.</p>
+            <ul class="social-links">
+                <li><a href="https://github.com/johndoe" target="_blank">GitHub</a></li>
+                <li><a href="https://linkedin.com/in/johndoe" target="_blank">LinkedIn</a></li>
+                <li><a href="https://twitter.com/johndoe" target="_blank">Twitter</a></li>
+            </ul>
+        </div>
+    </footer>
+    <script src="js/main.js"></script>
+</body>
+</html>
